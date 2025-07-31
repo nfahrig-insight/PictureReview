6df5f05682ed25475a009ad60a6b8221f6b792c3
Nick Fahrig <Nick.Fahrig@insight.com>
2025-07-31 16:34:53 -0400

    removing main method

--- PhotoAnalyzer/PhotoAnalyzer.Client/Program.cs
+++ PhotoAnalyzer/PhotoAnalyzer.Client/Program.cs
@@ -8,19 +8,3 @@ builder.Services.AddSingleton<PhotoAnalyzer.Client.Services.GraphServiceFactory>
 
 var host = builder.Build();
 await host.RunAsync();
-
-// Main method to run after the app has started
-await MainAsync(host);
-
-static async Task MainAsync(WebAssemblyHost host)
-{
-    var factory = host.Services.GetRequiredService<PhotoAnalyzer.Client.Services.GraphServiceFactory>();
-
-    var client = factory.CreateGraphServiceClient();
-
-    Console.WriteLine("GraphServiceClient created successfully.");
-    // Example: resolve a service and do something
-    // var graphFactory = host.Services.GetRequiredService<PhotoAnalyzer.Client.Services.GraphServiceFactory>();
-    // var client = graphFactory.CreateGraphServiceClient();
-    // await Task.CompletedTask;
-}
