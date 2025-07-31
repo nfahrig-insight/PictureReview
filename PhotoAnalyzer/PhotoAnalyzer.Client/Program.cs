498ec4c3e09e9ce02a25bab0da953632a063677f
Nick Fahrig <Nick.Fahrig@insight.com>
2025-07-31 17:07:45 -0400

    adding openid

--- PhotoAnalyzer/PhotoAnalyzer.Client/Program.cs
+++ PhotoAnalyzer/PhotoAnalyzer.Client/Program.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/.default");
+});
+
 builder.Services.Configure<PhotoAnalyzer.Client.Services.AzureAdOptions>(options =>
     builder.Configuration.GetSection("AzureAd").Bind(options)
 );
 builder.Services.AddSingleton<PhotoAnalyzer.Client.Services.GraphServiceFactory>();
 
-var host = builder.Build();
-await host.RunAsync();
+await builder.Build().RunAsync();
